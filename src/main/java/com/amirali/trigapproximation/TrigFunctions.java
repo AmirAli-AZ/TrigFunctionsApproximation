3fbcb03438ff1b5fe38c7836b99cca5a9f5d4043
Amir Ali Azimi <azimi6370@gmail.com>
2024-03-25 01:25:52 +0330

    update

--- src/main/java/com/amirali/trigapproximation/TrigFunctions.java
+++ src/main/java/com/amirali/trigapproximation/TrigFunctions.java
@@ -32,7 +32,7 @@ public class TrigFunctions {
         return sin/cos;
     }
 
-    public static long factorial(int n) {
+    private static long factorial(int n) {
         if (n == 0)
             return 1;
         return n*factorial(n - 1);
